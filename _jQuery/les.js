588f32b11a4703118adf42cd3d59a45edd3012ba
pverhaert <patrick.verhaert@thomasmore.be>
2014-05-01 09:50:39 +0200

    Update homepage

--- _jQuery/les.js
+++ _jQuery/les.js
@@ -1,10 +1,8 @@
 // JavaScript Document
 $(function() {
 	var codeVersie = $.fn.jquery;
-	//var isbn = '97890356.1282.2';
 	
 	$('.codeVersie').html(codeVersie);
-	//$('footer p').html('<a href="http://www.campiniamedia.be/fondslijst_detail.asp?ISBN=' + isbn + '">Efficiënt scripten met jQuery</a> versie: ' + codeVersie);
 	$('footer p').html('<a href="https://leanpub.com/jq11">Efficiënt scripten met jQuery</a> versie: ' + codeVersie);
 	$('a[href^="http://"]').filter('a:not([target])').attr('target','_blank');
 	
@@ -13,7 +11,7 @@ $(function() {
 	var thisAnker = thisPath.substr(thisPath.lastIndexOf('/'));
 	thisAnker = thisPath.substr(0, (thisPath.length - thisAnker.length));
 	thisAnker = thisAnker.substr(thisAnker.lastIndexOf('/') + 1);
-	$('#mainNav a#h').attr('href', $('#mainNav a').attr('href') + '#' + thisAnker);
+	(thisAnker == 'opdrachten') ? '' : $('#mainNav a#h').attr('href', $('#mainNav a').attr('href') + '#' + thisAnker);
 		
 	// Vorige pagina toevoegen
 	history.length > 1 ? $('#mainNav').prepend('<a href="javascript:;" id="hisBack">Vorige pagina</a> | ') :'' ;
